299c1de5c4c77b523105543c166a1915796277af
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-02 12:46:24 +0200

    Maze gameobjects are created in container in hierarchy.

--- Assets/_Project/Managers/GenerateMaze.cs
+++ Assets/_Project/Managers/GenerateMaze.cs
@@ -10,13 +10,17 @@ public class GenerateMaze : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
+        var dynamicObjects = GameObject.Find("_DynamicObjects");
+        var walls = new GameObject("Walls");
+        walls.transform.parent = dynamicObjects.transform;
+
 		for(int w = 0; w < width; w++)
 		{
 			for(int d = 0; d < depth; d++)
 			{
 				if( w == 0 || d == 0)	//outside walls bottom and left
 				{
-					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y, d + this.transform.position.z), Quaternion.identity);
+					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y, d + this.transform.position.z), Quaternion.identity, walls.transform);
 				}	
 				else if( w < 3 && d < 3)
 				{
@@ -24,11 +28,11 @@ public class GenerateMaze : MonoBehaviour {
 				}		
 				else if(w == width-1 || d == depth-1) //outside walls top and right
 				{
-					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity, walls.transform);
 				}
 				else if(Random.Range(0,5) < 1)
 				{
-					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity, walls.transform);
 				}
 			}	
 		}
